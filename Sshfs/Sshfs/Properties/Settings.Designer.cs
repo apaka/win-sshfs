0737ed5b1a9c4d34f7efd35e8b2ee4e26be2e204
Martin Dimov <dimov@4e1.cz>
2014-07-21 18:45:58 +0200

    Set timeouts to 0(off) Probably solves some troubles with git after commit/merge etc.

--- Sshfs/Sshfs/Properties/Settings.Designer.cs
+++ Sshfs/Sshfs/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Sshfs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public uint AttributeCacheTimeout {
             get {
                 return ((uint)(this["AttributeCacheTimeout"]));
@@ -34,7 +34,7 @@ namespace Sshfs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("60")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public uint DirContentCacheTimeout {
             get {
                 return ((uint)(this["DirContentCacheTimeout"]));
