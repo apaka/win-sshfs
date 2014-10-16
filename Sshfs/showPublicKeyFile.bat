37b66a5d524c04478f7ac72f8dadea8af0b4655f
Martin Dimov <dimov@4e1.cz>
2014-10-16 20:14:26 +0200

    Swap to public key without password

---
+++ Sshfs/showPublicKeyFile.bat
@@ -0,0 +1,4 @@
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -p sshfs\WinSSH4e1-public.snk WinSSH4e1.pub
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -tp WinSSH4e1.pub
+pause
+del WinSSH4e1.pub
