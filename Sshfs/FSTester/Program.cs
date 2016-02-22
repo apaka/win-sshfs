29ce99c5ee7ddaaafe44cc53bc98cef3eb0992cd
Martin Dimov <dimov@4e1.cz>
2016-02-22 01:35:24 +0100

    add simple FSTester

---
+++ Sshfs/FSTester/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSTester
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You need help. right?");
+                return;
+            }
+            else
+            {
+                string workingPath = args[0];
+
+                Collection<Test> tests = new Collection<Test>();
+                tests.Add(new TestWriteLines(workingPath));
+
+                foreach (Test test in tests)
+                {
+                    Console.Write("Test X: ");
+                    if (test.Go())
+                    {
+                        Console.WriteLine("Success");
+                    }
+                    else
+                    {
+                        Console.Write("Fail");
+                        Console.WriteLine(" >>> " + test.getLastError());
+                    }
+                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sshfs/FSTester/TestWriteLines.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FSTester
+{
+    public class TestWriteLines : Test
+    { 
+        public TestWriteLines(string workPath) : base(workPath) {}
+
+        override public bool Go()
+        {
+            if (!base.Go())
+            {
+                return false;
+            }
+
+            string tmp = Guid.NewGuid().ToString() + ".tmp";
+
+            FileStream f = File.Open(this.workingPath + " \\ " + tmp, FileMode.OpenOrCreate);
+            string s = "ABCD" + "\n";
+            byte[] sb = ASCIIEncoding.UTF8.GetBytes(s);
+            f.Write(sb, 0, sb.Length);
+            f.Seek(1, SeekOrigin.Begin);
+            f.Write(sb, 0, sb.Length);
+
+            f.Flush();
+            f.Seek(0, SeekOrigin.Begin);
+            byte[] data = new byte[4096];
+            int received = f.Read(data, 0, data.Length);
+            string ret = ASCIIEncoding.UTF8.GetString(data, 0, received);
+
+            if (ret == "A" + s)
+            {
+                return true;
+            }
+
+            this.lastError = "Got "+ret+", waited for "+ "A"+s;
+            return false;
+        }
+
+
+    }
+}
