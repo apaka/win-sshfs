f365a6a66bdce1fea4436f730c111221b81a9ce4
Martin Dimov <dimov@4e1.cz>
2016-10-19 23:20:46 +0200

    init issue tpl, add VS gitattr

--- Sshfs/Sshfs/Properties/AssemblyInfo.cs
+++ Sshfs/Sshfs/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("SFTP Filesystem for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Foreveryone")]
-[assembly: AssemblyProduct("WinSshFS Foreveryone 1.6.1.17-devel")]
+[assembly: AssemblyProduct("WinSshFS Foreveryone 1.6.1.18-devel")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.17")]
-[assembly: AssemblyVersion("1.6.1.17")]
-[assembly: AssemblyFileVersion("1.6.1.17")]
+// [assembly: AssemblyVersion("1.6.1.18")]
+[assembly: AssemblyVersion("1.6.1.18")]
+[assembly: AssemblyFileVersion("1.6.1.18")]
